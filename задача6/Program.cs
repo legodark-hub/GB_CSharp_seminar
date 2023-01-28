0edfafafbafde55c75f577cd7a99927a3849ecbc
aidarminyazev <legodark@mail.ru>
2023-01-28 22:27:48 +0500

    задача 6

---
+++ задача6/Program.cs
@@ -0,0 +1,6 @@
+﻿// Напишите программу, которая на вход принимает 
+// число и выдаёт, является ли число чётным
+Console.WriteLine("Введите число");
+int a = int.Parse(Console.ReadLine());
+if(a%2==0) Console.WriteLine($"Число {a} четное");
+else Console.WriteLine($"Число {a} нечетное");
