f0a45e9a14450d786f751d78c6623b3a9b2ac2e0
aidarminyazev <legodark@mail.ru>
2023-02-04 16:05:34 +0500

    задача23

---
+++ задача23/Program.cs
@@ -0,0 +1,16 @@
+﻿// Напишите программу, которая принимает на вход 
+// число (N) и выдаёт таблицу кубов чисел от 1 до N.
+Console.WriteLine("Введите число N: ");
+int N = int.Parse(Console.ReadLine());
+if (N>0) {
+    foreach (int value in Enumerable.Range(1, N)) {
+        Console.Write($"{Math.Pow(value, 3)} ");
+    }
+}
+else {
+    foreach (int value in Enumerable.Range(N, -N)) {
+        Console.Write($"{Math.Pow(value, 3)} ");
+    }
+}
+
+
