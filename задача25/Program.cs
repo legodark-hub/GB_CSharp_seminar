075e4477f42f3e94dd96a43861bb34e721fe9a71
aidarminyazev <legodark@mail.ru>
2023-02-08 19:56:27 +0500

    задача25

---
+++ задача25/Program.cs
@@ -0,0 +1,9 @@
+﻿// Напишите функцию, который принимает на вход два числа (A и B)
+//  и возводит число A в натуральную степень B.
+int Exponentiation(int a, int b) {
+    int result = 1;
+    for (int i=1; i<=b; i++) {
+        result*=a;
+    }
+    return result;
+}
