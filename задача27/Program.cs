4d351871bb11c20fa9df01b9adda0521363d6030
aidarminyazev <legodark@mail.ru>
2023-02-08 20:13:21 +0500

    задача27

---
+++ задача27/Program.cs
@@ -0,0 +1,10 @@
+﻿// Напишите программу, которая принимает на вход число и
+//  выдаёт сумму цифр в числе.
+int NumberSum (int num) {
+    int sum = 0;
+    while (num>0) {
+        sum = sum + num%10;
+        num/=10;
+    }
+    return sum;
+}
