e5ab4376d7536ba2fa188efce5811d3d1b933819
aidarminyazev <legodark@mail.ru>
2023-02-11 18:50:18 +0500

    задача36

---
+++ задача36/Program.cs
@@ -0,0 +1,18 @@
+﻿// Задайте одномерный массив,
+// заполненный случайными числами. Найдите сумму элементов, стоящих на нечётных позициях.
+
+int ArrayOddSum(int n) {
+    int[] array = new int[n];
+    int oddsum = 0;
+    for (int i = 0; i<array.Length; i++) {
+        array[i] = new Random().Next(-100, 100);
+        Console.Write($"{array[i]} ");
+        if (i%2!=0) oddsum+=array[i];
+    }
+    Console.WriteLine();
+    return oddsum;
+}
+
+Console.WriteLine("Введите размер массива");
+int n = int.Parse(Console.ReadLine());
+Console.WriteLine(ArrayOddSum(n));
