8492d3908d4310770230191e6a595fc9e35ffce6
aidarminyazev <legodark@mail.ru>
2023-02-14 20:24:16 +0500

    задача41

---
+++ задача41/Program.cs
@@ -0,0 +1,15 @@
+﻿// Пользователь вводит с клавиатуры M чисел. Посчитайте, 
+// сколько чисел больше 0 ввёл пользователь.
+
+int PositiveNums(double[] array) {
+    int counter = 0;
+    foreach (double a in array) {
+        if (a>0) counter++;
+    }
+    return counter;
+}
+
+Console.WriteLine("Введите набор чисел через пробел");
+string input = Console.ReadLine();
+double[] array = input.Split(' ').Select(Double.Parse).ToArray();
+Console.WriteLine(PositiveNums(array));
