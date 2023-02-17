ff1498112dbf93c5dea29e01b44f0804181770fd
aidarminyazev <legodark@mail.ru>
2023-02-17 20:08:19 +0500

    задача47

---
+++ задача47/Program.cs
@@ -0,0 +1,22 @@
+﻿// Задайте двумерный массив размером m×n, заполненный 
+// случайными вещественными числами.
+
+double[,] Generate2DArray(int[] array)
+{
+    double[,] result = new double[array[0], array[1]];
+    for (int i = 0; i < array[0]; i++)
+    {
+        for (int j = 0; j < array[1]; j++)
+        {
+            result[i, j] = new Random().NextDouble() * 10;
+            Console.Write($"{result[i, j]:0.00} ");
+        }
+        Console.WriteLine();
+    }
+    return result;
+}
+
+Console.WriteLine("Задайте число строк и стобцов массива через пробел");
+string input = Console.ReadLine();
+int[] array = input.Split(' ').Select(int.Parse).ToArray();
+Generate2DArray(array);
