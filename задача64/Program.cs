13dd93c173e696f57326d40a20e11ad7e1a4bb5a
Aidar Minyazev <54939061+legodark-hub@users.noreply.github.com>
2023-02-27 10:56:55 +0000

    задача64

---
+++ задача64/Program.cs
@@ -0,0 +1,13 @@
+﻿// Задайте значение N. Напишите программу, которая 
+// выведет все натуральные числа в промежутке от N до 1.
+void PrintNaturalNumbers(int n)
+{
+    if (n > 0)
+    {
+        Console.WriteLine(n);
+        PrintNaturalNumbers(n - 1);
+    }
+}
+
+Console.WriteLine("Ведите натуральное число");
+PrintNaturalNumbers(int.Parse(Console.ReadLine()));
