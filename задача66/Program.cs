fda48e28c5ada4a6e0b568c285c13a6e3f1b0159
Aidar Minyazev <54939061+legodark-hub@users.noreply.github.com>
2023-02-27 11:51:50 +0000

    задача66

---
+++ задача66/Program.cs
@@ -0,0 +1,14 @@
+﻿// Задайте значения M и N. Напишите программу, которая найдёт 
+// сумму натуральных элементов в промежутке от M до N.
+
+int NaturalNumbersSum(int m, int n, int sum = 0)
+{
+    if (m <= n) return NaturalNumbersSum(m+1,n,sum+m);
+    else return sum;
+}
+
+Console.WriteLine("Введите начальное число промежутка");
+int m = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите конечное число промежутка");
+int n = int.Parse(Console.ReadLine());
+Console.WriteLine(NaturalNumbersSum(m,n));
